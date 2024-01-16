2dcbc459b7e067f1ee4eb67685f0d2cb6118b723
MIkeEKB3d <kklennn@gmail.com>
2024-01-16 23:55:02 +0500

    KS-1-1 решение 1 задачи 1 урока

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -3,5 +3,5 @@ package org.example.lesson_1
 fun main() {
     println( "Это решение задачи 1 из  1 урока ")
 }
-val numberOfOrders = 75
-val gratitudeText = "Спасибо за покупку, Ваш промокод на следующий заказ 2323"
+val numberOfOrders: Int = 75
+val gratitudeText: String = "Спасибо за покупку, Ваш промокод на следующий заказ 2323"
