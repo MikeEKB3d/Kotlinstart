feb0f9fe6f8daa6e56198a6224d2853544bbfae9
MIkeEKB3d <kklennn@gmail.com>
2024-01-17 23:43:17 +0500

    KS-1-3 решение 3 задачи 1 урока

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -2,10 +2,14 @@ fun main(){
  val year = 1961
     var hour: String = "09"
     var minute: String = "07"
-println(year)
-    println(hour)
-    println(minute)
+    println(year)
+    //println(hour)
+    //println(minute)
 
+     hour = "10:"
+    minute = "55"
+    print(hour)
+    println(minute)
 
 
 }
