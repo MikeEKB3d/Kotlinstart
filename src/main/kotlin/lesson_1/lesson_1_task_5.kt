14f0678c23a78cdeb53501a995b375a3a4a72ff6
MIkeEKB3d <kklennn@gmail.com>
2024-01-18 00:32:31 +0500

    KS-1-5 решение 5 задачи 1 урока

---
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    var timeGagarin: String = "6480"
+ timeGagarin = "01"
+    val timeGagarinMinute: String = ":48"
+    val timeGagarinSecond: String = ":00"
+
+
+    print(timeGagarin)
+    print(timeGagarinMinute)
+    print(timeGagarinSecond)
+
+
+
+}
