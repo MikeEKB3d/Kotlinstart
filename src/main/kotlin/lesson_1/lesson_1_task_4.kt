916b4fb17bc21b9fa88bdbb0043a9de3fad4cc08
MIkeEKB3d <kklennn@gmail.com>
2024-01-18 00:13:03 +0500

    KS-1-4 решение 4 задачи 1 урока

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,18 @@
+fun main(){
+ val  length: Long = 40868600000
+ val age: Byte = 27
+ val partOfDay: Float = 0.075F
+ val second: Short = 6480
+ val partOfYear: Float = 2.0547945205479453E-4F
+ val apogee: Int = 327000
+
+ println(length)
+ println(age)
+ println(partOfDay)
+ println(second)
+ println(partOfYear)
+ println(apogee)
+
+
+
+}
