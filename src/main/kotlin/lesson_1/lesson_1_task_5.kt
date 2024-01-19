5b0e470cdd1fd3e727a406033b4fb84217bd20fa
MIkeEKB3d <kklennn@gmail.com>
2024-01-19 20:21:15 +0500

    KS-1-5 решение 5 задачи 1 урока

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -3,12 +3,13 @@ fun main() {
     val timeGagarin = 6480
     val timeGagarinMinuteAll = timeGagarin / 60
     val timeGagarinMinute = timeGagarinMinuteAll % 60
-    val timeGagarinHour = timeGagarinMinuteAll / 60
-    val timeGagarinSecond = timeGagarin % 60
+    val timeGagarinHour = String.format("%02d", timeGagarinMinuteAll / 60)
+    val timeGagarinSecond = String.format("%02d", timeGagarin % 60)
 
 
 
-    print("0$timeGagarinHour:$timeGagarinMinute:0$timeGagarinSecond")
+    println("$timeGagarinHour:$timeGagarinMinute:$timeGagarinSecond")
+
 
 
 }
