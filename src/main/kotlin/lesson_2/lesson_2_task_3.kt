6c5d1bf7bb8d87b42f2dec358cf7f92355d9d7e8
MIkeEKB3d <kklennn@gmail.com>
2024-01-20 22:38:12 +0500

    KS-2-3 решение 2 урока 3 задача

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -5,12 +5,8 @@ fun main() {
     val timeMinute = 39
     val arriveTimeMinute = 457
     val time = 60
-
-
     val timeEndMinuteAll = (timeHour * time) + timeMinute + arriveTimeMinute
-
     val timeEndHour = timeEndMinuteAll / time
-
     val timeEndMinute = timeEndMinuteAll % time
 
     println("$timeEndHour:$timeEndMinute")
