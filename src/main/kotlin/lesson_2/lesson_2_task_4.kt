82ec1ec44a0694e5857ac557d4689c144b2479a4
MIkeEKB3d <kklennn@gmail.com>
2024-01-20 15:33:31 +0500

    KS-2-4 решение 2 урока 4 задача

---
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_2
+
+fun main() {
+    val baff: Double = 0.2
+    val cristalOre = 7
+    val ironOre = 11
+    val cristRud = "Бонусная кристаллическая руда"
+    val ironRud = "Бонусная железная руда"
+    val space = ' '
+
+    val cristalBonus = (cristalOre * baff)
+    val ironBonus = ironOre * baff
+
+    println(cristRud + space + String.format("%.0f", cristalBonus))
+    println(ironRud + space + String.format("%.0f", ironBonus))
+
+}
