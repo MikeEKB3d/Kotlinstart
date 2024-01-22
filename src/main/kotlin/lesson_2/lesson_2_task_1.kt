237408a982d1053b1ad17d3852b51b6579bff923
MIkeEKB3d <kklennn@gmail.com>
2024-01-23 00:24:05 +0500

    KS-2-1 решение 2 урока 1 задача

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -6,9 +6,9 @@ fun main() {
     val scoreMen2 = 4
     val scoreMen3 = 3
     val scoreMen4 = 5
-    val sumScore = scoreMen1 + scoreMen2 + scoreMen3 + scoreMen4
+    val sumScore = (scoreMen1 + scoreMen2 + scoreMen3 + scoreMen4).toFloat()
     val average = (sumScore / allMen)
-    println("%+.2f".format(average.toFloat()))
+    println(average)
 
 
 
