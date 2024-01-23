612d24b5c44cac8f73a00adbbf58922d304f5ced
MIkeEKB3d <kklennn@gmail.com>
2024-01-23 15:42:44 +0500

    KS-3-4 решение 3 урока 4 задача

---
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+import java.awt.geom.Ellipse2D
+
+fun main() {
+    var nomerHoda: Int = 1
+    var otkudaHod = "E2"
+    var kudaHod = "E4"
+    var stroka = "$nomerHoda. $otkudaHod - $kudaHod"
+    println(stroka)
+    nomerHoda++
+
+}
