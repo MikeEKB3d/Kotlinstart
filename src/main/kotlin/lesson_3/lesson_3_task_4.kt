63a99786e74cc89041808bdccb9b896470966d15
MIkeEKB3d <kklennn@gmail.com>
2024-01-23 15:51:06 +0500

    KS-3-4 решение 3 урока 4 задача

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -6,8 +6,13 @@ fun main() {
     var nomerHoda: Int = 1
     var otkudaHod = "E2"
     var kudaHod = "E4"
-    var stroka = "$nomerHoda. $otkudaHod - $kudaHod"
+    var stroka = "${nomerHoda++}. $otkudaHod - $kudaHod"
     println(stroka)
-    nomerHoda++
+
+    otkudaHod = "D2"
+    kudaHod = "D3"
+    stroka = "${nomerHoda++}. $otkudaHod - $kudaHod"
+    println(stroka)
+
 
 }
