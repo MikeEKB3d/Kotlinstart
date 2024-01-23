ed83bd3a042e4563e538db1472254e44253f94b7
MIkeEKB3d <kklennn@gmail.com>
2024-01-23 13:49:27 +0500

    KS-3-3 решение 3 урока 3 задача

--- src/main/kotlin/lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -1,5 +1,20 @@
 package org.example.lesson_3
 
 fun main() {
+    val ishodnoeNumber = 7
+    var counter = 1
 
+    var tablica = """
+        $ishodnoeNumber * ${counter}  = ${ishodnoeNumber * counter++}
+        $ishodnoeNumber * ${counter} = ${ishodnoeNumber * counter++}
+        $ishodnoeNumber * ${counter} = ${ishodnoeNumber * counter++}
+        $ishodnoeNumber * ${counter} = ${ishodnoeNumber * counter++}
+        $ishodnoeNumber * ${counter} = ${ishodnoeNumber * counter++}
+        $ishodnoeNumber * ${counter} = ${ishodnoeNumber * counter++}
+        $ishodnoeNumber * ${counter} = ${ishodnoeNumber * counter++}
+        $ishodnoeNumber * ${counter} = ${ishodnoeNumber * counter++}
+        $ishodnoeNumber * ${counter} = ${ishodnoeNumber * counter++}
+   
+    """.trimIndent()
+    println(tablica)
 }
